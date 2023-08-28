edc08c4a841dab0df2c33a2b67d1efa263767896
xaco04 <xavivico04@gmail.com>
2023-08-28 16:56:47 +0200

    Creación reward-m-adm-admin

---
+++ src/app/components/admin/reward-m-adm-admin/reward-m-adm-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reward-m-adm-admin',
+  templateUrl: './reward-m-adm-admin.component.html',
+  styleUrls: ['./reward-m-adm-admin.component.css']
+})
+export class RewardMAdmAdminComponent {
+
+}
