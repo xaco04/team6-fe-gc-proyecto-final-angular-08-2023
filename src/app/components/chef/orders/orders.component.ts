4c0054d8ad9247c6fbb2d9c042c2c3b0c7997096
RobertoCarrera <robertocarrera93@gmail.com>
2023-09-05 18:09:18 +0200

    Corrección responsiveness
    Fallaba alguna cosita, full responsive, a falta de incorporar los comportamientos según tamaño de pantalla

--- src/app/components/chef/orders/orders.component.ts
+++ src/app/components/chef/orders/orders.component.ts
@@ -7,4 +7,69 @@ import { Component } from '@angular/core';
 })
 export class OrdersComponent {
 
+  dishes = [
+    {
+      nombre: "Ensalada Mixta",
+      image: '../../../../assets/croquetas.jpg'
+    },
+    {
+      nombre: "Pollo con patatas fritas",
+      image: '../../../../assets/croquetas.jpg'
+    },
+      {
+      nombre: "Agua",
+      image: '../../../../assets/croquetas.jpg'
+    },
+      {
+      nombre: "Tarta de queso",
+      image: '../../../../assets/croquetas.jpg'
+    },
+      {
+      nombre: "Patatas bravas",
+      image: '../../../../assets/croquetas.jpg'
+    }
+  ];
+
+  orders = [
+    {
+      n_pedido: 1,
+      hora_recogida: '12:30',
+      dishes: this.dishes
+    },
+    {
+      n_pedido: 2,
+      hora_recogida: '12:30',
+      dishes: this.dishes
+    },
+      {
+      n_pedido: 3,
+      hora_recogida: '12:45',
+      dishes: this.dishes
+    },
+      {
+      n_pedido: 4,
+      hora_recogida: '12:45',
+      dishes: this.dishes
+    },
+      {
+      n_pedido: 5,
+      hora_recogida: '12:45',
+      dishes: this.dishes
+    },
+      {
+      n_pedido: 6,
+      hora_recogida: '13:30',
+      dishes: this.dishes
+    },
+      {
+      n_pedido: 7,
+      hora_recogida: '13:30',
+      dishes: this.dishes
+    },
+      {
+      n_pedido: 8,
+      hora_recogida: '13:30',
+      dishes: this.dishes
+    },
+  ];
 }
