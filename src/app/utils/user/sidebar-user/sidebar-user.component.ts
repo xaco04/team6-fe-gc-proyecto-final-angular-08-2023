1c0bac76a1e148ecd268c594b3a042bd4bfdee28
RobertoCarrera <robertocarrera93@gmail.com>
2023-09-06 13:48:12 +0200

    Mejorando diseño
    Añadido el carrito, con scroll, mejorado responsiveness. Mejorado los iconos de menús

--- src/app/utils/user/sidebar-user/sidebar-user.component.ts
+++ src/app/utils/user/sidebar-user/sidebar-user.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-user',
   templateUrl: './sidebar-user.component.html',
   styleUrls: ['./sidebar-user.component.css']
 })
-export class SidebarUserComponent {
+export class SidebarUserComponent implements OnInit {
 
+  total: number = 0;
+
+  order = [
+    {
+      dish: 'Ensalada César',
+      quantity: 1,
+      price: 4.5
+    },
+    {
+      dish: 'Pollo con patatas fritas',
+      quantity: 1,
+      price: 6.5
+    },
+    {
+      dish: 'Coca Cola',
+      quantity: 1,
+      price: 2.5
+    },
+    {
+      dish: 'Tarta de queso',
+      quantity: 1,
+      price: 4.5
+    },
+    {
+      dish: 'Patatas bravas',
+      quantity: 1,
+      price: 3.8
+    }
+  ];
+
+  ngOnInit(): void {
+
+    this.getTotal();
+  }
+
+  getTotal(): number{
+
+    for(let i = 0; i < this.order.length; i++)
+    {
+      this.total += this.order[i].price*this.order[i].quantity;
+    }
+
+    return this.total
+  }
 }
