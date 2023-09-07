cd2cc49dd45774d0544da7d17ce47d96bb06b655
Joan de la Huerta <joandelahuertaplanas@gmail.com>
2023-09-07 20:19:39 +0200

    orders acabados listado

--- src/app/components/chef/orders/orders.component.ts
+++ src/app/components/chef/orders/orders.component.ts
@@ -30,46 +30,76 @@ export class OrdersComponent {
     }
   ];
 
-  orders = [
+  orders: any[] = [
     {
       n_pedido: 1,
       hora_recogida: '12:30',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
     {
       n_pedido: 2,
       hora_recogida: '12:30',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
       {
       n_pedido: 3,
       hora_recogida: '12:45',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
       {
       n_pedido: 4,
       hora_recogida: '12:45',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
       {
       n_pedido: 5,
       hora_recogida: '12:45',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
       {
       n_pedido: 6,
       hora_recogida: '13:30',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
       {
       n_pedido: 7,
       hora_recogida: '13:30',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
       {
       n_pedido: 8,
       hora_recogida: '13:30',
-      dishes: this.dishes
+      dishes: this.dishes, 
+      finished: false
     },
   ];
+
+  isOrderFinished: boolean = false;
+  showFinishedOrders: boolean = false;
+
+  toggleShowFinishedOrders() {
+    this.showFinishedOrders = !this.showFinishedOrders;
+  }
+
+  toggleAllOrdersFinished() {
+    this.orders.forEach(order => (order.finished = true));
+  }
+
+  getFinishedOrders() {
+    return this.orders.filter(order => order.finished);
+  }
+
+  toggleOrderFinished(order: any) {
+    order.finished = !order.finished;
+  }
+
+
+
 }
