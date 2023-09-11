53c76fed798fd6637e1ece41af49921e90199e73
xaco04 <xavivico04@gmail.com>
2023-09-11 10:33:43 +0200

    cambio en logica de recovery

--- src/app/components/shared/recovery/recovery.component.ts
+++ src/app/components/shared/recovery/recovery.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from 'src/app/services/shared/auth-service.service';
 
 @Component({
   selector: 'app-recovery',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class RecoveryComponent {
 
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthServiceService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
